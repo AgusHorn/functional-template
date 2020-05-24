0f97e13917c05ee02d939c815010d7bbba4f0400
Agustin Horn <agustin.horn@avature.net>
2020-05-24 15:56:55 -0300

    Excercise 9 finished

--- src/exercise9.clj
+++ src/exercise9.clj
@@ -1,18 +1,14 @@
 (ns exercise9)
+(require '[clojure.string :as str])
 
-(defn word-count 
+(defn word-count
   "Returns a map where each key is a distinct word in a given sentence and each value is the number of occurences of that word in the sentence."
   [x]
-  (let [words {}]
-       (doseq [word (clojure.string/split x #"\s")]
-           (if (contains? words word)
-               (update-in words [:word] inc)
-               (assoc-in words [:word] 1)
-           )
-           (println words)
-        )
-       (println words)
-   words)
+  (reduce (fn [words word]
+            (assoc words (str/lower-case word) (inc (get words (str/lower-case word) 0))))
+        {}
+        (re-seq #"\w+" x)
+  )
 )
 
 
